1788c1b3aab57a416e84e76cdff11978c68caed9
Evsyukov Denis <denis@evsyukov.org>
2022-01-07 21:21:43 +0300

    [+] add some logic
    * add redis
    * add some function for working with redis

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/juev/counter
 go 1.17
 
 require (
+	github.com/go-redis/redis/v8 v8.11.4
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.7.2
 	google.golang.org/grpc v1.43.0
 	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.2.0
@@ -10,11 +11,13 @@ require (
 )
 
 require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/golang/glog v1.0.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
+	golang.org/x/net v0.0.0-20210428140749-89ef3d95e781 // indirect
 	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
-	golang.org/x/text v0.3.5 // indirect
+	golang.org/x/text v0.3.6 // indirect
 	google.golang.org/genproto v0.0.0-20211208223120-3a66f561d7aa // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	sigs.k8s.io/yaml v1.3.0 // indirect
--- server/server.go
+++ server/server.go
@@ -6,15 +6,15 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"strconv"
 
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
-
+	"github.com/go-redis/redis/v8"
 	pb "github.com/juev/counter/proto/counter"
+	"google.golang.org/grpc"
 )
 
 var (
+	rdb  *redis.Client
 	port = flag.Int("port", 50051, "The server port")
 )
 
@@ -22,15 +22,39 @@ type Implementation struct {
 	pb.UnimplementedCounterServer
 }
 
-func (s *Implementation) AddDomain(context.Context, *pb.Domain) (*pb.Response, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method AddDomain not implemented")
+func (s *Implementation) AddDomain(ctx context.Context, domain *pb.Domain) (*pb.Response, error) {
+	if !keyExist(ctx, domain.GetDomain()) {
+		err := rdb.Set(ctx, domain.Domain, 0, 0).Err()
+		if err != nil {
+			return nil, err
+		}
+		return &pb.Response{
+			Status: `ok`,
+		}, nil
+	}
+	return &pb.Response{Status: `domain already exist`}, nil
 }
 
-func (s *Implementation) RemoveDomain(context.Context, *pb.Domain) (*pb.Response, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method RemoveDomain not implemented")
+func (s *Implementation) RemoveDomain(ctx context.Context, domain *pb.Domain) (*pb.Response, error) {
+	rdb.Del(ctx, domain.GetDomain())
+	return &pb.Response{
+		Status: `domain deleted`,
+	}, nil
 }
 
-func (s *Implementation) GetStat(context.Context, *pb.Domain) (*pb.Stats, error) {
+func (s *Implementation) GetStat(ctx context.Context, domain *pb.Domain) (*pb.Stats, error) {
+	if keyExist(ctx, domain.GetDomain()) {
+		val, err := rdb.Get(ctx, domain.GetDomain()).Result()
+		switch {
+		case err == redis.Nil:
+			return &pb.Stats{Cnt: 0}, nil
+		case err != nil:
+			log.Fatalf("cannot get `%s` key: %v", domain.GetDomain(), err)
+		default:
+			result, _ := strconv.Atoi(val)
+			return &pb.Stats{Cnt: int64(result)}, nil
+		}
+	}
 	log.Println("Get request /stats")
 	return &(pb.Stats{Cnt: 1}), nil
 }
@@ -39,7 +63,39 @@ func newCounterServer() *Implementation {
 	return &Implementation{}
 }
 
+func initRedis(ctx context.Context) {
+	rdb = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	if !keyExist(ctx, "any") {
+		err := rdb.Set(ctx, "any", 0, 0).Err()
+		if err != nil {
+			log.Fatalf("cannot set `any` key on redis: %v", err)
+		}
+	}
+}
+
+func keyExist(ctx context.Context, key string) bool {
+	_, err := rdb.Get(ctx, key).Result()
+	switch {
+	case err == redis.Nil:
+		return false
+	case err != nil:
+		log.Fatalf("cannot get `%s` key: %v", key, err)
+	default:
+		return true
+	}
+	return false
+}
+
 func main() {
+	ctx := context.Background()
+
+	initRedis(ctx)
+
 	flag.Parse()
 	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
 	if err != nil {
